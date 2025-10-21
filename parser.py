bf307893d144a1bd6a2c37524795e95d7e2a4e57
Choimoe <qwq.shq@gmail.com>
2025-10-21 19:24:28 +0800

    fix: 修复麻将记录解析器中的墙壁设置和发牌逻辑，优化了牌局信息的打印功能，注释掉了调试输出

--- parser.py
+++ parser.py
@@ -82,14 +82,18 @@ class MahjongRecordParser:
         wall_indices = [int(self.script_data['w'][i:i + 2], 16) for i in range(0, len(self.script_data['w']), 2)]
         dice = [self.script_data['d'] & 15, (self.script_data['d'] >> 4) & 15,
                 (self.script_data['d'] >> 8) & 15, (self.script_data['d'] >> 12) & 15]
+        # print(dice)
         dealer_idx = self.script_data['i'] % 4
 
         for p in range(4):
             if self.WIND[p] == '东':
                 dealer_idx = p
 
-        wall_break_pos = (dice[0] + dice[1] - 1) % 4
+        wall_break_pos = (dealer_idx - (dice[0] + dice[1] - 1) + 4) % 4
         start_pos = (wall_break_pos * 36) + (dice[0] + dice[1] + dice[2] + dice[3]) * 2
+        start_pos = start_pos % 144
+
+        # print(start_pos, wall_break_pos, dealer_idx)
 
         self.wall = wall_indices[start_pos:] + wall_indices[:start_pos]
         self.wall_back_ptr = len(self.wall) - 1
@@ -126,7 +130,7 @@ class MahjongRecordParser:
             f"手牌 {'✓' if g['o'] else '✕'} | 错和 鸣牌 {'✓' if g['d'] else '✕'} "
             f"{'-30/+10' if g['z'] else '-40/+0'} | {'随机座位' if g['r'] else '固定座位'}"
         )
-        print(config_str)
+        # print(config_str)
 
         start_time_unix = self.script_data['t'] / 1000
         end_time_unix = start_time_unix + (self.actions[-1]['t'] / 1000 if self.actions else 0)
@@ -134,23 +138,23 @@ class MahjongRecordParser:
         start_dt = datetime.fromtimestamp(start_time_unix, tz).strftime('%Y-%m-%d %H:%M:%S UTC%z')
         end_dt = datetime.fromtimestamp(end_time_unix, tz).strftime('%Y-%m-%d %H:%M:%S UTC%z')
 
-        print(f"开始时间：{start_dt}")
-        print(f"结束时间：{end_dt}")
+        # print(f"开始时间：{start_dt}")
+        # print(f"结束时间：{end_dt}")
 
-        print("\n--- 玩家信息 ---")
-        for i in range(4):
-            print(f"{self.WIND[i]}家: {p_info[i]['n']} (分数: {p_info[i]['s']})")
+        # print("\n--- 玩家信息 ---")
+        # for i in range(4):
+        #     print(f"{self.WIND[i]}家: {p_info[i]['n']} (分数: {p_info[i]['s']})")
 
     def run_analysis(self):
         self._print_game_info()
         self._setup_wall_and_deal()
 
-        print("\n--- 初始配牌 ---")
-        for i in range(4):
-            hand_str = ' '.join(self.initial_hands[i])
-            print(f"{self.WIND[i]}家 {self.script_data['p'][i]['n']}:\t {hand_str}")
+        # print("\n--- 初始配牌 ---")
+        # for i in range(4):
+        #     hand_str = ' '.join(self.initial_hands[i])
+        #     print(f"{self.WIND[i]}家 {self.script_data['p'][i]['n']}:\t {hand_str}")
 
-        print("\n--- 对局过程 ---")
+        # print("\n--- 对局过程 ---")
         prev_time = 0
 
         for act in self.actions:
@@ -166,6 +170,8 @@ class MahjongRecordParser:
             elif a_type == 1:
                 self.flower_counts[p_idx] += 1
                 ot = (hi_byte & 15) + 136
+                # print(self.hands[p_idx])
+                # print(ot)
                 self.hands[p_idx].remove(ot)
                 self.hands[p_idx].append(lo_byte)
                 output += f"{'自动' if data & 0x1000 else '手动'}补花 {self.get_tile_str(ot)}->{self.get_tile_str(lo_byte)}"
@@ -186,6 +192,9 @@ class MahjongRecordParser:
 
                 if pack_type == "CHI":
                     offer_tile = self.last_discard_info['tile']
+                    # print(tile_val, offer_tile)
+                    if tile_val - 4 + ((data >> 10) & 3) < 0:
+                        tile_val = offer_tile
                     c1 = tile_val - 4 + ((data >> 10) & 3)
                     c2 = tile_val + ((data >> 12) & 3)
                     c3 = tile_val + 4 + ((data >> 14) & 3)
@@ -193,15 +202,22 @@ class MahjongRecordParser:
                     chi_id = 1
                     chi_shape = []
                     # chi_tiles.remove(offer_tile)
+                    # print([self.get_tile_str(t) for t in chi_tiles])
                     for t in range(3):
-                        if chi_tiles[t] == offer_tile:
+                        if chi_tiles[t] >> 2 == offer_tile >> 2:
                             chi_id = t + 1
                             chi_shape.append(f"({self.get_tile_str(chi_tiles[t])})")
                         else:
-                            self.hands[p_idx].remove(chi_tiles[t])
+                            # print(self.hands[p_idx], chi_tiles)
+                            for _ in self.hands[p_idx]:
+                                if (_ >> 2) == (chi_tiles[t] >> 2):
+                                    self.hands[p_idx].remove(_)
+                                    break
+                            # self.hands[p_idx].remove(chi_tiles[t])
                             chi_shape.append(self.get_tile_str(chi_tiles[t]))
                     self.packs[p_idx].append(("CHI", self.get_tile_GB_str(tile_val), chi_id))
                     self.packs_output[p_idx].append(chi_shape)
+                    print(self.packs_output[p_idx])
                     output += f"吃 {self.get_tile_str(offer_tile)}"
                 elif pack_type == "PENG":
                     count = 0
@@ -217,24 +233,39 @@ class MahjongRecordParser:
                     self.last_action_was_kong = True
                     action = "加杠" if (data & 0x0300) == 0x0300 else "杠"
                     if action == "加杠":
-                        self.hands[p_idx].remove(tile_val)
+                        # self.hands[p_idx].remove(tile_val)
+                        hand_copy = list(self.hands[p_idx])
+                        for t in hand_copy:
+                            if (t >> 2) == (tile_val >> 2):
+                                self.hands[p_idx].remove(t)
+                                break
                         for i, (pt, pstr, po) in enumerate(self.packs[p_idx]):
                             if pt == "PENG" and pstr == self.get_tile_str(tile_val):
                                 self.packs[p_idx][i] = ("GANG", pstr, po);
                                 self.packs_output[p_idx][i] = ([self.get_tile_str(tile_val) for _ in range(4)])
                                 break
                     elif (data >> 6) & 3 == 0:  # Concealed
-                        for _ in range(4):
-                            self.hands[p_idx].remove(tile_val)
+                        count = 0
+                        hand_copy = list(self.hands[p_idx])
+                        for t in hand_copy:
+                            if (t >> 2) == (tile_val >> 2) and count < 4:
+                                self.hands[p_idx].remove(t)
+                                count += 1
+                        print([self.get_tile_str(t) for t in self.hands[p_idx]])
                         self.packs[p_idx].append(("GANG", self.get_tile_GB_str(tile_val), 0))
                         self.packs_output[p_idx].append([self.get_tile_str(tile_val) for _ in range(4)])
                     else:  # Melded
                         for _ in range(3):
-                            self.hands[p_idx].remove(tile_val)
+                            count = 0
+                            for t in self.hands[p_idx]:
+                                if (t >> 2) == (tile_val >> 2) and count < 3:
+                                    self.hands[p_idx].remove(t)
+                                    count += 1
+                            # self.hands[p_idx].remove(tile_val)
                         self.packs[p_idx].append(("GANG", self.get_tile_GB_str(tile_val), (data >> 6) & 3))
                         self.packs_output[p_idx].append([self.get_tile_str(tile_val) for _ in range(4)])
                     output += f"{action} {self.get_tile_str(tile_val)}"
-                self.discards[offer_from_idx].pop()
+                # self.discards[offer_from_idx].pop()
             elif a_type == 6:
                 is_self_drawn = p_idx == self.current_player_idx
                 win_tile = self.hands[p_idx][-1] if is_self_drawn else self.last_discard_info['tile']
@@ -253,7 +284,10 @@ class MahjongRecordParser:
                 output += "过"
             elif a_type == 9:
                 output += "弃"
-            # print(f"{output} {time_str}")
+            print(f"{output} {time_str}")
+            hand = [self.get_tile_str(t) for t in self.hands[p_idx]]
+            hand.sort()
+            print(hand)
             prev_time = time
 
         print("\n--- 最终结果 ---")
@@ -262,10 +296,10 @@ class MahjongRecordParser:
         else:
             self._calculate_fan()
 
-        print("\n--- 各家舍牌 ---")
-        for i in range(4):
-            discard_str = ' '.join([self.get_tile_str(t) for t in self.discards[i]])
-            print(f"{self.WIND[i]}家 {self.script_data['p'][i]['n']}: {discard_str}")
+        # print("\n--- 各家舍牌 ---")
+        # for i in range(4):
+        #     discard_str = ' '.join([self.get_tile_str(t) for t in self.discards[i]])
+        #     print(f"{self.WIND[i]}家 {self.script_data['p'][i]['n']}: {discard_str}")
 
         print("\n--- 最终手牌 ---")
         for i in range(4):
@@ -292,20 +326,27 @@ class MahjongRecordParser:
         for p_packs in self.packs:
             for p_type, p_tile_str, _ in p_packs:
                 if p_type == "PENG":
-                    all_tiles_revealed.extend([p_tile_str] * 3)
+                    all_tiles_revealed.extend([p_tile_str] * 2)
                 elif p_type == "GANG":
-                    all_tiles_revealed.extend([p_tile_str] * 4)
+                    all_tiles_revealed.extend([p_tile_str] * 3)
 
-        print(all_tiles_revealed)
-        print(self.get_tile_str(self.win_info['win_tile']))
-        print(all_tiles_revealed.count(self.get_tile_str(self.win_info['win_tile'])))
-        is_4th_tile = all_tiles_revealed.count(self.get_tile_str(self.win_info['win_tile'])) >= 3
+        all_tiles_revealed.sort()
+        # print(all_tiles_revealed)
+        # print(f"{self.WIND[w_idx]}家 {self.script_data['p'][w_idx]['n']} " + ("自摸：" if self.win_info['is_self_drawn'] else "点和：") + self.get_tile_str(self.win_info['win_tile']))
+        # print(all_tiles_revealed.count(self.get_tile_str(self.win_info['win_tile'])))
+        tiles_num = all_tiles_revealed.count(self.get_tile_str(self.win_info['win_tile']))
+        is_4th_tile = False
+        if tiles_num == 3 and self.win_info['is_self_drawn']:
+            is_4th_tile = True
+        if tiles_num == 4:
+            is_4th_tile = True
 
         calculator_args = {
             'pack': tuple(self.packs[w_idx]),
             'hand': tuple(hand_tiles),
             'winTile': win_tile_str,
-            'flowerCount': self.flower_counts[w_idx],
+            # 'flowerCount': self.flower_counts[w_idx],
+            'flowerCount': 0,
             'isSelfDrawn': self.win_info['is_self_drawn'],
             'is4thTile': is_4th_tile,
             'isAboutKong': self.last_action_was_kong,
